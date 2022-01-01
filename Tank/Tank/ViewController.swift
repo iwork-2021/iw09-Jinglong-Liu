32efb60bc8f1fe912d4d0b5ba2328a4c792c0264
Jinglong-Liu <1601029287@qq.com>
2022-01-01 10:28:56 +0800

    move

--- Tank/Tank/ViewController.swift
+++ Tank/Tank/ViewController.swift
@@ -19,8 +19,27 @@ class ViewController: UIViewController {
         // Load the "Box" scene from the "Experience" Reality File
         //let boxAnchor = try! TinyToyTank.loadBox()
         tankAnchor = try! TinyToyTank.load_TinyToyTank()
+        tankAnchor!.turret?.setParent(
+          tankAnchor!.tank, preservingWorldTransform: true)
+
         // Add the box anchor to the scene
         //arView.scene.anchors.append(boxAnchor)
         arView.scene.anchors.append(tankAnchor!)
+    }
+    @IBAction func cannonFire(_ sender: Any) {
+    }
+    @IBAction func tankForward(_ sender: Any) {
+        tankAnchor!.notifications.tankForward.post()
+    }
+    @IBAction func tankLeft(_ sender: Any) {
+        tankAnchor!.notifications.tankLeft.post()
+    }
+    @IBAction func tankRight(_ sender: Any) {
+        tankAnchor!.notifications.tankRight.post()
+    }
+    
+    @IBAction func turretLeft(_ sender: Any) {
+    }
+    @IBAction func turretRight(_ sender: Any) {
     }
 }
